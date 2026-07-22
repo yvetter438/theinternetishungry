ff0125f36b6561a7f6a8f949705e183f259e772f
yvetter438 <yannick.vetter@outlook.com>
2026-07-22 16:20:03 -0700

    mistake

--- little-thai-restaurant/menu.js
+++ little-thai-restaurant/menu.js
@@ -12,8 +12,8 @@ window.MENU_CONFIG = {
         { id: "4cdf99d2-be7a-4b65-bb68-df0a9623c937", name: "Phad See Ew" },
         { id: "a9bd6789-cd06-49e5-b46a-42e6f011e5c0", name: "Phad Thai" },
         { id: "bd4c510e-402e-422a-b0f6-29d3e262ac47", name: "House Fried Rice" },
-        { id: "db3f3c0c-eca6-4d15-a973-9ca21c10253c", name: "Thai Spring Rolls" },
-        { id: "42886560-95cb-4464-90e5-cc2800dae4c3", name: "Spring Rolls" },
+        { id: "db3f3c0c-eca6-4d15-a973-9ca21c10253c", name: "Fresh Rolls" },
+        { id: "42886560-95cb-4464-90e5-cc2800dae4c3", name: "Thai Spring Rolls" },
         { id: "ca661888-cb24-4c7c-bccc-45d424568bd7", name: "Corn Patties" }
     ],
     menuUrl: "https://littlethairestaurantwa.smiledining.com/?2026070601",
