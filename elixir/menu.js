dd871f3ebc2cac71dcb2f93bb34112e6d0959295
yvetter438 <yannick.vetter@outlook.com>
2026-07-21 19:50:42 -0700

    item names and adn order link

--- elixir/menu.js
+++ elixir/menu.js
@@ -8,13 +8,13 @@ window.MENU_CONFIG = {
     showOrderButton: true,
     ctaLabel: "Full menu",
     videos: [
-        { id: "93c7c4aa-7190-4098-bff9-2dd887fbb906" },
-        { id: "a5348504-0d72-4352-8cc0-02d07a364a66" },
-        { id: "8f30c8d5-933c-431d-89be-35e1ecfdfe9a" },
-        { id: "dd0761ff-0369-4131-a150-029deac8784c" },
-        { id: "4380cb40-aab6-4e82-976a-6f58971bce13" },
-        { id: "edbaa141-d0ca-4890-b0e9-a7f96bcc0db0" }
+        { id: "93c7c4aa-7190-4098-bff9-2dd887fbb906", name: "Thai Sweet Potato Fries" },
+        { id: "a5348504-0d72-4352-8cc0-02d07a364a66", name: "Steak Spring Salad" },
+        { id: "8f30c8d5-933c-431d-89be-35e1ecfdfe9a", name: "Gochujang Mole Chicken Tacos" },
+        { id: "dd0761ff-0369-4131-a150-029deac8784c", name: "Pho Beef Sliders" },
+        { id: "4380cb40-aab6-4e82-976a-6f58971bce13", name: "Kimchi Mac and Cheese" },
+        { id: "edbaa141-d0ca-4890-b0e9-a7f96bcc0db0", name: "Raspberry Yuzu Soda" }
     ],
-    menuUrl: "https://www.elixirseattle.net/",
-    takeoutUrl: "https://www.elixirseattle.net/"
+    menuUrl: "https://order.toasttab.com/online/elixir-cafe-4524-university-way-ne",
+    takeoutUrl: "https://order.toasttab.com/online/elixir-cafe-4524-university-way-ne"
 };
