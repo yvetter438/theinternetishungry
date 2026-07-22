fae13c7046f43bd3a292b6aa25ac40ad6cc9846f
yvetter438 <yannick.vetter@outlook.com>
2026-07-22 16:17:25 -0700

    updated Little thai Restaurant

--- little-thai-restaurant/menu.js
+++ little-thai-restaurant/menu.js
@@ -8,13 +8,13 @@ window.MENU_CONFIG = {
     showOrderButton: true,
     videos: [
         { id: "85b7b9c0-0a01-4112-bb04-6965392d6f0f", name: "All Day Combo" },
-        { id: "593d7286-e7be-4b00-ab34-87f83310dd44" },
-        { id: "4cdf99d2-be7a-4b65-bb68-df0a9623c937" },
-        { id: "a9bd6789-cd06-49e5-b46a-42e6f011e5c0" },
-        { id: "bd4c510e-402e-422a-b0f6-29d3e262ac47" },
-        { id: "db3f3c0c-eca6-4d15-a973-9ca21c10253c", name: "Fresh Rolls" },
-        { id: "42886560-95cb-4464-90e5-cc2800dae4c3" },
-        { id: "ca661888-cb24-4c7c-bccc-45d424568bd7" }
+        { id: "593d7286-e7be-4b00-ab34-87f83310dd44", name: "Khao Krapow" },
+        { id: "4cdf99d2-be7a-4b65-bb68-df0a9623c937", name: "Phad See Ew" },
+        { id: "a9bd6789-cd06-49e5-b46a-42e6f011e5c0", name: "Phad Thai" },
+        { id: "bd4c510e-402e-422a-b0f6-29d3e262ac47", name: "House Fried Rice" },
+        { id: "db3f3c0c-eca6-4d15-a973-9ca21c10253c", name: "Thai Spring Rolls" },
+        { id: "42886560-95cb-4464-90e5-cc2800dae4c3", name: "Spring Rolls" },
+        { id: "ca661888-cb24-4c7c-bccc-45d424568bd7", name: "Corn Patties" }
     ],
     menuUrl: "https://littlethairestaurantwa.smiledining.com/?2026070601",
     takeoutUrl: "https://littlethairestaurantwa.smiledining.com/?2026070601"
